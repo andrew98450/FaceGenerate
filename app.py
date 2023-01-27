c75a9ab757d49ecf3775e94125c8c4b2e26281ac
Andrew Huang <ktpss96034@gmail.com>
2023-01-27 14:59:10 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import torch
 import matplotlib.pyplot as plt
 from flask import *
-from wgan import NetG
+from wgan import *
 
 app = Flask(__name__)
 
@@ -10,33 +10,17 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         n = int(request.form['n'])
-        model = str(request.form['model'])
+        gender = str(request.form['gender'])
         if not os.path.exists("static"):
             os.mkdir("static")
-        image_path = f"static/{model}_image.jpg"
-        if model in "WGAN":
-            generator_model = torch.load("Pretrained/wgan_modelG.pth")
+        image_path = f"static/wgangp_image.jpg"
+        generator_model = torch.load("Pretrained/wgangp_modelG.pth")
+
+        if gender in "Female":
             batch_size = n ** 2
             inputs = torch.randn(batch_size, 128)
-            outputs = generator_model(inputs)
-
-            plt.figure()
-            for i in range(1, batch_size + 1):
-                image = 0.5 + outputs[i - 1] * 0.5
-                image = image * 255.0
-                image = image.permute(1, 2, 0)
-                image = image.byte().numpy()
-                plt.subplot(n, n, i)
-                plt.axis('off')
-                plt.imshow(image)
-
-            plt.tight_layout()
-            plt.savefig(image_path)
-        elif model in "WGAN-GP":
-            generator_model = torch.load("Pretrained/wgangp_modelG.pth")
-            batch_size = n ** 2
-            inputs = torch.randn(batch_size, 128)
-            outputs = generator_model(inputs)
+            labels = torch.randint(low=0, high=1, size=(batch_size,))
+            outputs = generator_model(inputs, labels)
 
             plt.figure()
             for i in range(1, batch_size + 1):
@@ -51,7 +35,45 @@ def index():
             plt.tight_layout()
             plt.savefig(image_path)
 
-        return render_template("index.html", view_image=True, model=model, n=n)
+        elif gender in "Male":
+            batch_size = n ** 2
+            inputs = torch.randn(batch_size, 128)
+            labels = torch.randint(low=1, high=2, size=(batch_size,))
+            outputs = generator_model(inputs, labels)
+
+            plt.figure()
+            for i in range(1, batch_size + 1):
+                image = 0.5 + outputs[i - 1] * 0.5
+                image = image * 255.0
+                image = image.permute(1, 2, 0)
+                image = image.byte().numpy()
+                plt.subplot(n, n, i)
+                plt.axis('off')
+                plt.imshow(image)
+
+            plt.tight_layout()
+            plt.savefig(image_path)
+
+        elif gender in "Random":
+            batch_size = n ** 2
+            inputs = torch.randn(batch_size, 128)
+            labels = torch.randint(low=0, high=2, size=(batch_size,))
+            outputs = generator_model(inputs, labels)
+
+            plt.figure()
+            for i in range(1, batch_size + 1):
+                image = 0.5 + outputs[i - 1] * 0.5
+                image = image * 255.0
+                image = image.permute(1, 2, 0)
+                image = image.byte().numpy()
+                plt.subplot(n, n, i)
+                plt.axis('off')
+                plt.imshow(image)
+
+            plt.tight_layout()
+            plt.savefig(image_path)
+
+        return render_template("index.html", view_image=True, gender=gender, n=n)
     return render_template("index.html", view_image=False)
 
 if __name__ == '__main__':
--- wgan.py
+++ wgan.py
@@ -3,10 +3,9 @@ class NetG(torch.nn.Module):
 
     def __init__(self) -> None:
         super(NetG, self).__init__()
+        self.embedding_layer = torch.nn.Embedding(2, 128)
         self.fc_layer = torch.nn.Sequential(
-            torch.nn.Linear(128, 256 * 4 * 4, bias=False),
-            torch.nn.BatchNorm1d(256 * 4 * 4),
-            torch.nn.ReLU())
+            torch.nn.Linear(256, 256 * 4 * 4, bias=False))
         self.conv_layer = torch.nn.Sequential(
             torch.nn.Upsample(scale_factor=2),
             torch.nn.Conv2d(256, 128, 3, padding=1, bias=False),
@@ -20,7 +19,9 @@ class NetG(torch.nn.Module):
             torch.nn.Conv2d(64, 3, 3, padding=1, bias=False),
             torch.nn.Tanh())
     
-    def forward(self, inputs):
+    def forward(self, inputs, labels):
+        labels = self.embedding_layer(labels)
+        inputs = torch.cat([inputs, labels], dim=1)
         outputs = self.fc_layer(inputs)
         outputs = outputs.reshape((-1, 256, 4, 4))
         outputs = self.conv_layer(outputs)
